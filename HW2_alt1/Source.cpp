f4c85443e8f189883954bad79c77c50711be3218
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-09-21 21:24:11 -0800

    Assign value

--- HW2_alt1/Source.cpp
+++ HW2_alt1/Source.cpp
@@ -16,29 +16,42 @@ using std::vector;
 //this function takes the input from the user
 void enterCoins(vector<string>& coins) {
 	for (int i = 0; i < 1; i++) {
-		string amount;
+		string penny;
+		string nickel;
+		string dime;
+		string quarter;
+		string dollar;
 		cout << "How many pennies do you have: ";
-		getline(cin, amount);
-		coins.push_back(amount);
+		getline(cin, penny);
+		coins.push_back(penny);
 		cout << endl;
 		cout << "How many nickels do you have: ";
-		getline(cin, amount);
-		coins.push_back(amount);
+		getline(cin, nickel);
+		coins.push_back(nickel);
 		cout << endl;
 		cout << "How many dimes do you have: ";
-		getline(cin, amount);
-		coins.push_back(amount);
+		getline(cin, dime);
+		coins.push_back(dime);
 		cout << endl;
 		cout << "How many quarters do you have: ";
-		getline(cin, amount);
-		coins.push_back(amount);
+		getline(cin, quarter);
+		coins.push_back(quarter);
 		cout << endl;
 		cout << "How many dollar coins do you have: ";
-		getline(cin, amount);
-		coins.push_back(amount);
+		getline(cin, dollar);
+		coins.push_back(dollar);
 		cout << endl;
 	}
 	return;
+}
+
+void coinAmount(vector<string>& coins) {
+	string penny;
+	string nickel;
+	string dime;
+	string quarter;
+	string dollar;
+
 }
 
 int main() {
